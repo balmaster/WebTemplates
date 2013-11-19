ae534c8d4ba0bb483d1396bd496ea8452679316a
kolesja <les@svitweb.co.uk>
2013-11-20 01:44:07 +0200

    form elements rendering

---
+++ src/main/webapp/js/data.js
@@ -0,0 +1,238 @@
+var items= [
+            {
+                "attributeId":6607,
+                "attributeSource":"GROUP",
+                "defaultValue":"",
+                "error":null,
+                "fieldType":"STRING",
+                "grid":null,
+                "gridHead":null,
+                "help":"",
+                "hidden":false,
+                "inputMask":"",
+                "key":false,
+                "masks":[
+
+                ],
+                "name":"CNNUM",
+                "printInCheck":false,
+                "promt":"Текст",
+                "readOnly":false,
+                "regexps":[
+                    {
+                        "QName":{
+                            "localPart":"RegExp",
+                            "namespaceURI":"",
+                            "prefix":""
+                        },
+                        "comment":"",
+                        "errorMessage":"Значение должно содержать не более 4 цифр",
+                        "name":"Номер",
+                        "orderNum":0,
+                        "regexp":"^.{0,256}$"
+                    }
+                ],
+                "required":true,
+                "sizeMax":3,
+                "sizeMin":2,
+                "type":{
+                    "QName":{
+                        "localPart":"Type",
+                        "namespaceURI":"",
+                        "prefix":""
+                    },
+                    "code":"STRING",
+                    "regexps":[
+                        {
+                            "QName":{
+                                "localPart":"RegExp",
+                                "namespaceURI":"",
+                                "prefix":""
+                            },
+                            "comment":"",
+                            "errorMessage":"Значение должно содержать ",
+                            "name":"Номер ",
+                            "orderNum":0,
+                            "regexp":"^.{0,256}$"
+                        }
+                    ],
+                    "rules":[
+
+                    ]
+                },
+                "value":"1254",
+                "valueField":"",
+                "viewMask":""
+            },
+    {
+        "attributeId":6603,
+        "attributeSource":"GROUP",
+        "defaultValue":"",
+        "error":null,
+        "fieldType":"NUMBER",
+        "grid":null,
+        "gridHead":null,
+        "help":"",
+        "hidden":false,
+        "inputMask":"",
+        "key":false,
+        "masks":[
+
+        ],
+        "name":"NUMBER",
+        "printInCheck":false,
+        "promt":"Число",
+        "readOnly":false,
+        "regexps":[
+            {
+                "QName":{
+                    "localPart":"RegExp",
+                    "namespaceURI":"",
+                    "prefix":""
+                },
+                "comment":"",
+                "errorMessage":"Значение должно содержать не более 4 цифр",
+                "name":"Номер",
+                "orderNum":0,
+                "regexp":"^.{0,256}$"
+            }
+        ],
+        "required":true,
+        "sizeMax":"",
+        "sizeMin":"",
+        "type":{
+            "QName":{
+                "localPart":"Type",
+                "namespaceURI":"",
+                "prefix":""
+            },
+            "code":"STRING",
+            "regexps":[
+                {
+                    "QName":{
+                        "localPart":"RegExp",
+                        "namespaceURI":"",
+                        "prefix":""
+                    },
+                    "comment":"",
+                    "errorMessage":"Значение должно содержать ",
+                    "name":"Номер ",
+                    "orderNum":0,
+                    "regexp":"^.{0,256}$"
+                }
+            ],
+            "rules":[
+
+            ]
+        },
+        "value":"1254",
+        "valueField":"",
+        "viewMask":""
+    },
+    {
+        "attributeId":6603,
+        "attributeSource":"GROUP",
+        "defaultValue":"",
+        "error":null,
+        "fieldType":"MONEY",
+        "grid":null,
+        "gridHead":null,
+        "help":"",
+        "hidden":false,
+        "inputMask":"",
+        "key":false,
+        "masks":[  ],
+        "name":"MONEY",
+        "printInCheck":false,
+        "promt":"Деньги",
+        "readOnly":false,
+        "regexps":[ ],
+        "required":true,
+        "sizeMax":"",
+        "sizeMin":"",
+        "type":{
+            "QName":{
+                "localPart":"Type",
+                "namespaceURI":"",
+                "prefix":""
+            },
+            "code":"MONEY",
+            "regexps":[ ],
+            "rules":[ ]
+        },
+        "value":"1254.54",
+        "valueField":"",
+        "viewMask":""
+    },
+    {
+        "attributeId":6603,
+        "attributeSource":"GROUP",
+        "defaultValue":"",
+        "error":null,
+        "fieldType":"DATE",
+        "grid":null,
+        "gridHead":null,
+        "help":"",
+        "hidden":false,
+        "inputMask":"",
+        "key":false,
+        "masks":[  ],
+        "name":"DATE",
+        "printInCheck":false,
+        "promt":"Дата",
+        "readOnly":false,
+        "regexps":[ ],
+        "required":true,
+        "sizeMax":"",
+        "sizeMin":"",
+        "type":{
+            "QName":{
+                "localPart":"Type",
+                "namespaceURI":"",
+                "prefix":""
+            },
+            "code":"DATE",
+            "regexps":[ ],
+            "rules":[ ]
+        },
+        "value":"12.12.2012",
+        "valueField":"",
+        "viewMask":""
+    },
+    {
+        "attributeId":6603,
+        "attributeSource":"GROUP",
+        "defaultValue":"",
+        "error":null,
+        "fieldType":"TIME",
+        "grid":null,
+        "gridHead":null,
+        "help":"",
+        "hidden":false,
+        "inputMask":"",
+        "key":false,
+        "masks":[  ],
+        "name":"TIME",
+        "printInCheck":false,
+        "promt":"Время",
+        "readOnly":false,
+        "regexps":[ ],
+        "required":true,
+        "sizeMax":"",
+        "sizeMin":"",
+        "type":{
+            "QName":{
+                "localPart":"Type",
+                "namespaceURI":"",
+                "prefix":""
+            },
+            "code":"TIME",
+            "regexps":[ ],
+            "rules":[ ]
+        },
+        "value":"11/20/2013 1:39 АМ",
+        "valueField":"",
+        "viewMask":""
+    }
+
+];
--- src/main/webapp/js/payment.js
+++ src/main/webapp/js/payment.js
@@ -22,6 +22,47 @@ function initPayment(data) {
                     { collapsible: false, resizable:false}
                 ]
         });
+
+        var fields = new kendo.data.DataSource({ data: items });
+        var model = new kendo.observable({ fields: fields });
+        var view = new kendo.View("#field_list_template", {
+            model: model
+        });
+        $("#serv_attrs").html(view.render());
+
+        $("#attrsForm").kendoValidator({
+            messages: {
+                required: "Поле {0} не может быть пустым",
+                maxLength: "Максимальная длинна поля - {1} симв.",
+                minLength: "Минимальная длинна поля - {1} симв."
+            },
+            rules: {
+                maxLength: function(input) {
+                    var maxLength = input.attr("maxLength");
+
+                    if (!maxLength)
+                        return true;
+                    return input.val().length <= maxLength;
+                },
+                minLength: function(input) {
+                    var minLength = input.attr("minLength");
+
+                    if (!minLength)
+                        return true;
+                    return input.val().length >= minLength;
+                }
+            }
+        });
+
+        $("#attrsForm input[type=number]").kendoNumericTextBox({
+            format:"######.##"
+        });
+        $("#attrsForm input[type=money]").kendoNumericTextBox({
+            format: "#.00 грн"
+        });
+        $("#attrsForm input[type=date]").kendoDatePicker();
+
+        $("#attrsForm input[type=time]").kendoDateTimePicker();
     }
 };
 
